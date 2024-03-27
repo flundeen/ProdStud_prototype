f8b1c536fc9b5af1ace1f6b9d64e3537c71d12d2
Aldric Kirby Saligo <43484785+KitKirby21@users.noreply.github.com>
2024-03-27 13:46:19 -0400

    Fixed package visual bugs and drop issues
    Package visuals have been fixed to now properly represent whether a package is present or not. Package drop bug where package is picked up by dead car is fixed.

--- Assets/Karting/Scripts/PackageDelivery/Package.cs
+++ Assets/Karting/Scripts/PackageDelivery/Package.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Package : MonoBehaviour
 {
 
+    private float timer = 5.0f;
+
     // Update is called once per frame
     void Update()
     {
-        
+        timer -= Time.deltaTime;
+        if (timer <= 0f)
+        {
+            GameManager.Instance.packageIsPresent = false;
+            Destroy(gameObject);
+        }
     }
 
     private void OnTriggerEnter(Collider collision)
